4027daeac49d505a93110f160258968a6f8d0463
Neto Test <tne.2222@mail.ru>
2025-03-16 16:27:08 +0300

    HomeWorks2

---
+++ Урок 1/Задание 2/Задание 2/Задание 2/Задание 2.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <iostream>
+
+struct bank {
+	int account_number;
+	std::string name_account;
+	double money;
+};
+
+void increment_money(bank& b) {
+	std::cout << "Введите новую сумму:";
+	std::cin >> b.money;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	bank b;
+	std::cout << "Введите номер счёта:";
+	std::cin >> b.account_number;
+
+	std::cout << "Введите имя владельца:";
+	std::cin >> b.name_account;
+
+	std::cout << "Введите сумму:";
+	std::cin >> b.money;
+
+	increment_money(b);
+
+	std::cout << "Ваш счёт:" << b.name_account << ", " << b.account_number << ", " << b.money << std::endl;
+
+	return 0;
+
+}
