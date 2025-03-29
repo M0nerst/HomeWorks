ac17f6f705d7c6ae698c086c9e41acc9378f0d92
Neto Test <tne.2222@mail.ru>
2025-03-29 17:21:43 +0300

    дз Классы и объекты

---
+++ Классы и объекты/Задание 2/Задание 2/Задание 2.cpp
@@ -0,0 +1,109 @@
+﻿
+#include <iostream>
+#include <cstring>
+
+
+class Counter 
+{
+private:
+    int counterResult_ = 0;
+
+public:
+    Counter(int initialInitValue);
+    int getCounter();
+    int counterPlus();
+    int counterMinus();
+};
+
+Counter::Counter(int initialInitValue) 
+{
+    counterResult_ = initialInitValue;
+};
+
+int Counter::getCounter() 
+{
+    return counterResult_;
+};
+
+int Counter::counterPlus() 
+{
+    return ++counterResult_;
+};
+
+int Counter::counterMinus() 
+{
+    return --counterResult_;
+};
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    system("chcp 1251");
+
+
+    std::string checkInitialInitValue;
+    int initialInitValue = 0;
+    bool checkTrueInitialInitValue = false;
+
+
+    do
+    {
+        std::cout << std::endl << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+        std::cin >> checkInitialInitValue;
+        std::cout << std::endl;
+
+        if (checkInitialInitValue == "да" || checkInitialInitValue == "Да")
+        {
+            std::cout << "Введите начальное значение счётчика: ";
+            std::cin >> initialInitValue;
+            checkTrueInitialInitValue = true;
+        }
+        else if (checkInitialInitValue == "нет" || checkInitialInitValue == "Нет")
+        {
+            checkTrueInitialInitValue = true;
+        }
+        else
+        {
+            std::cout << "Вы ввели неверную команду.";
+        }
+    } while (!checkTrueInitialInitValue);
+
+    Counter Counter(initialInitValue);
+
+    std::cout << std::endl;
+
+    std::string commandValue;
+    bool checkCommand = false;
+
+    
+    do
+    {
+        std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+        std::cin >> commandValue;
+
+        if (commandValue == "x" || commandValue == "х")
+        {
+            std::cout << "До запуска!";
+            checkCommand = true;
+        }
+        else if (commandValue == "+")
+        {
+            Counter.counterPlus();
+        }
+        else if (commandValue == "-")
+        {
+            Counter.counterMinus();
+        }
+        else if (commandValue == "=")
+        {
+            std::cout << "Значение счетчика: " << Counter.getCounter() << "." << std::endl;
+        }
+        else
+        {
+            std::cout << "Вы ввели неверную команду." << std::endl;
+        }
+    } while (!checkCommand);
+}
