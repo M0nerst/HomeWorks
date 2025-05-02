7688660336beee9f8efe25be1a0e8beedf7a68c0
Neto Test <tne.2222@mail.ru>
2025-05-02 16:26:47 +0300

    Preprocessor and macros

---
+++ Preprocessor and macros/Task 1/Task 1/Task 1.cpp
@@ -0,0 +1,49 @@
+﻿
+#include <iostream>
+
+
+
+void add(double &FirstNumber, double &SecondNumber) {
+        std::cout << "Введите число 1: "; std::cin >> FirstNumber;
+     
+        std::cout << std::endl;
+        std::cout << "Введите число 2: "; std::cin >> SecondNumber;
+        
+        std::cout << std::endl;
+        std::cout << "Результат сложения:" << FirstNumber + SecondNumber;
+}
+
+#define MODE 1
+#if !defined MODE
+#error The MODE must be defined
+#endif
+
+int main()
+{
+    setlocale(LC_ALL, "Russian"); 
+    system("chcp 1251");
+    system("cls");
+
+#ifdef MODE
+
+    #if MODE == 0
+        std::cout << "Работаю в режиме тренировки" << std::endl;
+
+    #elif MODE == 1
+        std::cout << "Работаю в боевом режиме" << std::endl;
+        double FirstNumber = 0;
+        double SecondNumber = 0;
+        add(FirstNumber, SecondNumber);
+
+    #else
+        std::cout << "Неизвестный режим. Завершение работы" << std::endl;
+    #endif 
+
+#endif
+
+
+
+
+
+ 
+}
