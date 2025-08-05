2839810274b7099885242bcf29a5923e318be9cb
Neto Test <tne.2222@mail.ru>
2025-08-05 20:17:45 +0300

    Object lifecycle. RAII 1.0

---
+++ Object lifecycle. RAII/Task 1/FileName.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include <iostream>
+#include <string>
+
+class SmartArray
+{
+private:
+	int* smArray_ = 0;
+	int countForPushBack_ = 0;
+	int sizeOfArray_ = 0;
+
+public:
+	SmartArray(const int sizeOfArray);
+	~SmartArray();
+
+	SmartArray(const SmartArray&) = delete;
+	SmartArray& operator=(const SmartArray&) = delete;
+
+	void smartArrayPrint();
+	void smartArrayPushBack(const int addingElement);
+	int smartArrayGetElement(const int gettingElementByIndex);
+
+};
