33ee806c70f16457b82798393d437c341d5ab56f
Neto Test <tne.2222@mail.ru>
2025-10-22 19:14:09 +0300

    STL Part 1 1.0

---
+++ STL Part 1/Task 1/Task 1.cpp
@@ -0,0 +1,42 @@
+﻿
+#include <iostream>
+#include <algorithm>  
+#include <map>        
+#include <set>        
+
+// Функция для вывода результатов подсчета частоты символов
+void print(std::multimap<int, char, std::greater<int>>& map1, std::string& symbols) {
+    std::cout << "[IN]: " << symbols << "\n";
+    std::cout << "[OUT]: " << "\n";
+    for (const auto& m : map1) 
+    {
+        std::cout << m.second << " : " << m.first << "\n";
+    }
+}
+
+int main()
+{
+    // Исходная строка для анализа
+    std::string symbols{ "123555Helllo world!!!!!" };
+    int repet = 0;  // Переменная для хранения количества повторений символа
+
+    std::multimap<int, char, std::greater<int>> map1;
+
+     std::set<char> set1;
+
+    // Заполняем set уникальными символами из исходной строки
+    for (int i = 0; i < symbols.size(); i++) {
+        set1.insert(symbols[i]);
+    }
+
+    // Для каждого уникального символа подсчитываем его частоту в исходной строке
+    for (const auto& i : set1) 
+    {
+        repet = std::count(symbols.begin(), symbols.end(), i);
+        map1.emplace(repet, i);
+    }
+
+    print(map1, symbols);
+
+    return 0;
+}
---
+++ STL Part 1/Task 2/Task 2.cpp
@@ -0,0 +1,39 @@
+﻿
+#include <iostream>  
+#include <set>       
+
+
+void print(std::set<int, std::greater<>>& set1) 
+{
+    std::cout << "[OUT]:" << std::endl;
+    // Перебираем все элементы множества и выводим их
+    // Используем const auto& для избежания копирования
+    for (const auto& i : set1) 
+    {
+        std::cout << i << "\n";
+    }
+}
+
+int main()
+{
+    
+    std::set<int, std::greater<>> set1;
+    int size;  // Количество чисел для ввода
+
+    // Запрашиваем у пользователя количество чисел
+    std::cout << "[IN]:" << std::endl;
+    std::cin >> size;
+
+    // Читаем числа и добавляем их в множество
+    for (int i = 0; i < size; i++) 
+    {
+        int num = 0;
+        std::cin >> num;
+        set1.insert(num);  // Вставляем число в множество
+    }
+
+
+    print(set1);
+
+    return 0;  
+}
