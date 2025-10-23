8c7ba2b525bd840172199b5f059906613d32d2ab
Neto Test <tne.2222@mail.ru>
2025-10-23 13:55:38 +0300

    STL Part 2 1.0

---
+++ STL Part 2/Task 1/Task 1.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <iostream>  
+#include <vector>    
+#include <algorithm>  
+
+// Шаблонная функция для удаления дубликатов из вектора
+template<class T>
+void cleaning_elements(std::vector<T>& arr) {
+    std::sort(arr.begin(), arr.end());  // Сортируем вектор
+    auto last = std::unique(arr.begin(), arr.end());  // Удаляем дубликаты
+    arr.erase(last, arr.end());  // Обрезаем вектор до уникальных элементов
+}
+
+// Шаблонная функция для вывода элементов вектора
+template<class T>
+void print(std::vector<T>& arr) {
+    for (const auto n : arr) {  // Перебираем все элементы вектора
+        std::cout << n << " ";  // Выводим текущий элемент
+    }
+    std::cout << std::endl;  // Завершаем строку вывода
+}
+
+int main() {
+    // Инициализация вектора с дубликатами
+    std::vector<int> arr{ 1, 1, 2, 5, 6, 1, 2, 4 };
+    std::cout << "[IN]: ";  
+    print(arr);  
+
+    cleaning_elements(arr);  // Удаляем дубликаты из вектора
+    std::cout << "[OUT]: ";  
+    print(arr);  
+}
---
+++ STL Part 2/Task 2/Task 2.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <iostream> 
+#include <set>      
+#include <list>     
+#include <vector>   
+
+// Шаблонная функция для печати элементов контейнера
+template<class T>
+void print_container(T& container) {
+    for (const auto i : container) { // Перебираем элементы контейнера
+        std::cout << i << " "; // Выводим текущий элемент
+    }
+    std::cout << std::endl; // Завершаем строку вывода
+}
+
+int main()
+{
+   
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set); // Выводим элементы набора 
+
+    
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list); // Выводим элементы списка
+
+    
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector); // Выводим элементы вектора
+}
